61ab40a2bb7e9d3ab85d0e818ae194ad9430b563
Václav Makeš <info@makes.cz>
2014-11-23 12:37:20 +0100

    Modify structure of database
    For saving articles, categories and portals.

--- database/create-schema.sql
+++ database/create-schema.sql
@@ -2,6 +2,36 @@ CREATE DATABASE IF NOT EXISTS wikipedia;
 
 USE wikipedia;
 
-CREATE TABLE pages (
-	name VARCHAR(100)
-);
+
+CREATE TABLE articles (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255)
+) TYPE=innodb;
+
+
+CREATE TABLE categories (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255)
+) TYPE=innodb;
+
+
+CREATE TABLE portals (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255)
+) TYPE=innodb;
+
+
+CREATE TABLE article_portals (
+	article_id INT NOT NULL,
+	portal_id INT NOT NULL,
+	FOREIGN KEY (article_id) REFERENCES articles(id),
+	FOREIGN KEY (portal_id) REFERENCES portals(id)
+) TYPE=innodb;
+
+
+CREATE TABLE article_categories (
+	article_id INT NOT NULL,
+	category_id INT NOT NULL,
+	FOREIGN KEY (article_id) REFERENCES articles(id),
+	FOREIGN KEY (category_id) REFERENCES categories(id)
+) TYPE=innodb;
