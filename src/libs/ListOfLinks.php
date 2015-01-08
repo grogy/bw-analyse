52138ecdf138c135b49d530cdb3306031b684c1c
Václav Makeš <info@makes.cz>
2015-01-08 17:41:36 +0100

    Added parse list of all dumps + tests
    Parse list of all Wikimedia dumps (all language versions + project,
    for example Wikipedia, Wikibooks, etc.)

---
+++ src/libs/ListOfLinks.php
@@ -0,0 +1,62 @@
+<?php
+
+use Atrox\Matcher;
+use Nette\Database\Connection;
+
+class ListOfLinks
+{
+    /**
+     * @var Connection
+     */
+    private $database;
+
+    private $baseUrl;
+
+
+    public function __construct(Connection $database, $baseUrl)
+    {
+        $this->database = $database;
+        $this->baseUrl = $baseUrl;
+    }
+
+
+    public function update()
+    {
+        // @todo
+    }
+
+
+    public function getLanguagePages()
+    {
+        $html = file_get_contents($this->baseUrl);
+        $m = Matcher::multi('//li[not(@class)]', [
+            'text' => '.',
+            'title' => 'a',
+            'url' => 'a/@href',
+            'done' => 'span',
+        ])->fromHtml();
+        $parseData = $m($html);
+        array_walk($parseData, function(&$value, $key){
+            $date = substr($value['text'], 0, 10);
+            $value['time'] = new DateTime($date, new DateTimeZone('Europe/London'));
+            if (isset($value['done'])) {
+                $value['done'] = ($value['done'] == 'Dump complete') ? true : false;
+            } else {
+                $value['done'] = false;
+            }
+        });
+        $data = [];
+        foreach ($parseData as $item) {
+            print_r($item);
+            if (empty($item['title'])) {
+                continue;
+            }
+            $data[$item['title']] = [
+                'url' => $item['url'],
+                'time' => $item['time'],
+                'done' => $item['done'],
+            ];
+        }
+        return $data;
+    }
+}
---
+++ tests/ListOfLinks.phpt
@@ -0,0 +1,15 @@
+<?php
+
+use Tester\Assert;
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../src/libs/ListOfLinks.php';
+Tester\Environment::setup();
+
+$databaseMock = \Mockery::mock('Nette\Database\Connection')->makePartial();
+$listOfLinks = new ListOfLinks($databaseMock,  __DIR__ . '/input/list-of-languages.html');
+
+
+Assert::same('cswiki/20150105', $listOfLinks->getLanguagePages()['cswiki']['url']);
+Assert::type('DateTime', $listOfLinks->getLanguagePages()['cswiki']['time']);
+Assert::true($listOfLinks->getLanguagePages()['cswiki']['done']);
+Assert::false($listOfLinks->getLanguagePages()['dewiki']['done']);
