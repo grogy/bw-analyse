6c8f31fefbe8f7f1285690614e5497548ac207cc
Václav Makeš <info@makes.cz>
2015-07-17 16:07:22 +0200

    Update deploy script

--- dev/deploy-example.sh
+++ dev/deploy-example.sh
@@ -6,12 +6,12 @@ scp all.tar root@127.0.0.1:all.tar
 ssh root@127.0.0.1 <<'ENDSSH'
 rm -rf src/
 tar xvf all.tar
-sudo rm -rf /opt/wikipedia-analyse
-sudo mkdir /opt/wikipedia-analyse
-sudo mv src /opt/wikipedia-analyse/src
-sudo mv vendor /opt/wikipedia-analyse/vendor
-sudo mkdir /opt/wikipedia-analyse/temp
-sudo chmod -R a+rw /opt/wikipedia-analyse/temp
+rm -rf /opt/wikipedia-analyse
+mkdir /opt/wikipedia-analyse
+mv src /opt/wikipedia-analyse/src
+mv vendor /opt/wikipedia-analyse/vendor
+mkdir /opt/wikipedia-analyse/temp
+chmod -R a+rw /opt/wikipedia-analyse/temp
 rm all.tar
 ENDSSH
 
